426cab29b0e1b9aee9c1c9f49436e0c6536f823e
khrizLlano <criss0498@yahoo.es>
2023-02-25 15:41:10 -0500

    Estructura de consulta de ticket v.0.1

--- view/MainHead/head.php
+++ view/MainHead/head.php
@@ -9,10 +9,19 @@
 	<link href="../../public/img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
 	<link href="../../public/img/favicon.png" rel="icon" type="image/png">
 	<link href="../../public/img/favicon.ico" rel="shortcut icon">
+
     <link rel="stylesheet" href="../../public/css/lib/font-awesome/font-awesome.min.css">
     <link rel="stylesheet" href="../../public/css/lib/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="../../public/css/main.css">
+
+	<!-- setilos para el cuadro de descripcion -->
 	<link rel="stylesheet" href="../../public/css/lib/summernote/summernote.css"/>
 	<link rel="stylesheet" href="../../public/css/separate/pages/editor.min.css">
+
+	<!-- estilos para las notificaciones de registro -->
 	<link rel="stylesheet" href="../../public/css/lib/bootstrap-sweetalert/sweetalert.css">
 	<link rel="stylesheet" href="../../public/css/separate/vendor/sweet-alert-animations.min.css">
+
+	<!-- estilos para las tablas -->
+	<link rel="stylesheet" href="../../public/css/lib/datatables-net/datatables.min.css">
+	<link rel="stylesheet" href="../../public/css/separate/vendor/datatables-net.min.css">
--- view/MainJS/js.php
+++ view/MainJS/js.php
@@ -3,5 +3,12 @@
 	<script src="../../public/js/lib/bootstrap/bootstrap.min.js"></script>
 	<script src="../../public/js/plugins.js"></script>
     <script src="../../public/js/app.js"></script>
+
+	<!-- Script para habilitar cuadro de descripcion -->
 	<script src="../../public/js/lib/summernote/summernote.min.js"></script>
+
+	<!-- Script para las notificaciones -->
 	<script src="../../public/js/lib/bootstrap-sweetalert/sweetalert.min.js"></script>
+
+	<!-- Script para las tablas -->
+	<script src="../../public/js/lib/datatables-net/datatables.min.js"></script>
