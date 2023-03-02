ae29f9a94c6c466da040f0745e3d148d6be11ffe
khrizLlano <criss0498@yahoo.es>
2023-03-02 09:29:33 -0500

    Encriptacion de contraseña v.0.4

--- models/Usuario.php
+++ models/Usuario.php
@@ -11,7 +11,7 @@
                     header("Location:".Conectar::ruta()."index.php?m=2");
                     exit();
                 }else{
-                    $sql = "SELECT * FROM tm_usuario WHERE usu_correo=? and usu_pass=? and rol_id=? and est=1";
+                    $sql = "SELECT * FROM tm_usuario WHERE usu_correo=? and usu_pass=MD5(?) and rol_id=? and est=1";
                     $stmt=$conectar->prepare($sql);
                     $stmt->bindValue(1, $correo);
                     $stmt->bindValue(2, $pass);
@@ -36,7 +36,7 @@
         public function insert_usuario($usu_nom,$usu_ape,$usu_correo,$usu_pass,$rol_id){
             $conectar= parent::conexion();
             parent::set_names();
-            $sql="INSERT INTO  tm_usuario (usu_id, usu_nom, usu_ape, usu_correo, usu_pass, rol_id, fech_crea , fecha_mod, fecha_elim, est) VALUES (NULL, ?, ?, ?, ?, ?, now(), NULL, NULL, '1');";
+            $sql="INSERT INTO  tm_usuario (usu_id, usu_nom, usu_ape, usu_correo, usu_pass, rol_id, fech_crea , fecha_mod, fecha_elim, est) VALUES (NULL, ?, ?, ?, MD5(?), ?, now(), NULL, NULL, '1');";
             $sql=$conectar->prepare($sql);
             $sql->bindValue(1,$usu_nom);
             $sql->bindValue(2,$usu_ape);
@@ -86,7 +86,7 @@
         public function get_usuario(){
             $conectar= parent::conexion();
             parent::set_names();
-            $sql="SELECT * FROM tm_usuario where est='1';";
+            $sql="call sp_get_usuario()";
             $sql=$conectar->prepare($sql);
             $sql->execute();
             return $resultado=$sql->fetchAll();
@@ -95,7 +95,7 @@
         public function get_usuario_x_id($usu_id){
             $conectar= parent::conexion();
             parent::set_names();
-            $sql="SELECT * FROM tm_usuario where usu_id=?;";
+            $sql="call sp_get_usuario_x_id(?)";
             $sql=$conectar->prepare($sql);
             $sql->bindValue(1,$usu_id);
             $sql->execute();
