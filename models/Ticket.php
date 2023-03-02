e3c5341c981191e13049fdce10e134c62809c02c
khrizLlano <criss0498@yahoo.es>
2023-03-02 12:53:27 -0500

    Funcionalidad para Adjuntar Archivos a los Tickets v.0.4

--- models/Ticket.php
+++ models/Ticket.php
@@ -11,7 +11,11 @@
             $sql->bindValue(3,$tick_titulo);
             $sql->bindValue(4,$tick_descrip);
             $sql->execute();
-            return $resultado=$sql->fetchAll();
+
+            $sql1="select last_insert_id() as 'tick_id';";
+            $sql1=$conectar->prepare($sql1);
+            $sql1->execute();
+            return $resultado=$sql1->fetchAll();
         }
 
         public function listar_ticket_x_usu($usu_id){
