408b0dfd3ef15d51aed4dd96f0d0ab5cf2c9c9f7
khrizLlano <criss0498@yahoo.es>
2023-03-04 15:38:58 -0500

    Funcionalidad Filtrar Ticket v.0.2

--- view/ConsultarTicket/consultarticket.js
+++ view/ConsultarTicket/consultarticket.js
@@ -10,17 +10,25 @@ function init(){
 
 $(document).ready(function(){
 
+    /* Llenar Combo Categoria */
+    $.post("../../controller/categoria.php?op=combo",function(data, status){
+        $('#cat_id').html(data);
+    });
+
     /* LLenar Combo usuario asignar */
     $.post("../../controller/usuario.php?op=combo", function (data) {
         $('#usu_asig').html(data);
     });
 
-    /* TODO: llenar Combo Prioridad */
+    /* llenar Combo Prioridad */
     $.post("../../controller/prioridad.php?op=combo",function(data, status){
         $('#prio_id').html(data);
     });
     
-    if(rol_id==1){
+    /* rol si es 1 entonces es usuario */
+    if (rol_id==1){
+
+        $('#viewuser').hide();
 
         tabla=$('#ticket_data').dataTable({
             "aProcessing": true,
@@ -75,63 +83,15 @@ $(document).ready(function(){
                 }
             }     
         }).DataTable();
-        
     }else{
+        /* Filtro avanzado en caso de ser soporte */
+        var tick_titulo = $('#tick_titulo').val();
+        var cat_id = $('#cat_id').val();
+        var prio_id = $('#prio_id').val();
 
-        tabla=$('#ticket_data').dataTable({
-            "aProcessing": true,
-            "aServerSide": true,
-            dom: 'Bfrtip',
-            "searching": true,
-            lengthChange: false,
-            colReorder: true,
-            buttons: [
-                    'copyHtml5',
-                    'excelHtml5',
-                    'csvHtml5',
-                    'pdfHtml5'
-                    ],
-            "ajax":{
-                url: '../../controller/ticket.php?op=listar',
-                type : "post",
-                dataType : "json",
-                error: function(e){
-                    console.log(e.responseText);
-                }
-            },
-            "ordering": false,
-            "bDestroy": true,
-            "responsive": true,
-            "bInfo":true,
-            "iDisplayLength": 10,
-            "autoWidth": false,
-            "language": {
-                "sProcessing":     "Procesando...",
-                "sLengthMenu":     "Mostrar _MENU_ registros",
-                "sZeroRecords":    "No se encontraron resultados",
-                "sEmptyTable":     "Ningún dato disponible en esta tabla",
-                "sInfo":           "Mostrando un total de _TOTAL_ registros",
-                "sInfoEmpty":      "Mostrando un total de 0 registros",
-                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-                "sInfoPostFix":    "",
-                "sSearch":         "Buscar:",
-                "sUrl":            "",
-                "sInfoThousands":  ",",
-                "sLoadingRecords": "Cargando...",
-                "oPaginate": {
-                    "sFirst":    "Primero",
-                    "sLast":     "Último",
-                    "sNext":     "Siguiente",
-                    "sPrevious": "Anterior"
-                },
-                "oAria": {
-                    "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                }
-            }     
-        }).DataTable();
-
+        listardatatable(tick_titulo,cat_id,prio_id);
     }
+    
 });
 
 /* Mostrar datos antes de asignar */
@@ -213,6 +173,109 @@ function CambiarEstado(tick_id){
             });
         }
     });
+}
+
+/* Filtro avanzado */
+$(document).on("click","#btnfiltrar", function(){
+    limpiar();
+
+    var tick_titulo = $('#tick_titulo').val();
+    var cat_id = $('#cat_id').val();
+    var prio_id = $('#prio_id').val();
+
+    listardatatable(tick_titulo,cat_id,prio_id);
+});
+
+/* Restaurar Datatable js y limpiar */
+$(document).on("click","#btntodo", function(){
+    limpiar();
+
+    $('#tick_titulo').val('');
+    $('#cat_id').val('').trigger('change');
+    $('#prio_id').val('').trigger('change');
+
+    listardatatable('','','');
+});
+
+/* Listar datatable con filtro avanzado */
+function listardatatable(tick_titulo,cat_id,prio_id){
+    tabla=$('#ticket_data').dataTable({
+        "aProcessing": true,
+        "aServerSide": true,
+        dom: 'Bfrtip',
+        "searching": true,
+        lengthChange: false,
+        colReorder: true,
+        buttons: [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5'
+                ],
+        "ajax":{
+            url: '../../controller/ticket.php?op=listar_filtro',
+            type : "post",
+            dataType : "json",
+            data:{ tick_titulo:tick_titulo,cat_id:cat_id,prio_id:prio_id},
+            error: function(e){
+                console.log(e.responseText);
+            }
+        },
+        "bDestroy": true,
+        "responsive": true,
+        "bInfo":true,
+        "iDisplayLength": 10,
+        "autoWidth": false,
+        "language": {
+            "sProcessing":     "Procesando...",
+            "sLengthMenu":     "Mostrar _MENU_ registros",
+            "sZeroRecords":    "No se encontraron resultados",
+            "sEmptyTable":     "Ningún dato disponible en esta tabla",
+            "sInfo":           "Mostrando un total de _TOTAL_ registros",
+            "sInfoEmpty":      "Mostrando un total de 0 registros",
+            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":    "",
+            "sSearch":         "Buscar:",
+            "sUrl":            "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":     "Último",
+                "sNext":     "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }     
+    }).DataTable().ajax.reload();
+}
+
+/* Limpiamos restructurando el html del datatable js */
+function limpiar(){
+    $('#table').html(
+        "<table id='ticket_data' class='table table-bordered table-striped table-vcenter js-dataTable-full'>"+
+            "<thead>"+
+                "<tr>"+
+                    "<th style='width: 5%;'>Nro.Ticket</th>"+
+                    "<th style='width: 15%;'>Categoria</th>"+
+                    "<th class='d-none d-sm-table-cell' style='width: 30%;'>Titulo</th>"+
+                    "<th class='d-none d-sm-table-cell' style='width: 5%;'>Prioridad</th>"+
+                    "<th class='d-none d-sm-table-cell' style='width: 5%;'>Estado</th>"+
+                    "<th class='d-none d-sm-table-cell' style='width: 10%;'>Fecha Creación</th>"+
+                    "<th class='d-none d-sm-table-cell' style='width: 10%;'>Fecha Asignación</th>"+
+                    "<th class='d-none d-sm-table-cell' style='width: 10%;'>Fecha Cierre</th>"+
+                    "<th class='d-none d-sm-table-cell' style='width: 10%;'>Soporte</th>"+
+                    "<th class='text-center' style='width: 5%;'></th>"+
+                "</tr>"+
+            "</thead>"+
+            "<tbody>"+
+
+            "</tbody>"+
+        "</table>"
+    );
 }
 
 init();
